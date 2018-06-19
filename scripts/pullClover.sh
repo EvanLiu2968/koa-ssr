16e540d482cffc4a3e69b328af6fd7e5ec880d2a
evanliu2968 <evanliu2968@gmail.com>
2018-06-19 22:11:44 +0800

    delete extra code.

--- scripts/pullClover.sh
+++ scripts/pullClover.sh
@@ -18,13 +18,3 @@ else
 cd ../clover
 git pull
 fi
-
-# -e filename  如果 filename存在，则为真  [ -e /var/log/syslog ]
-# -d filename  如果 filename为目录，则为真  [ -d /tmp/mydir ]
-# -f filename  如果 filename为常规文件，则为真  [ -f /usr/bin/grep ]
-# -L filename  如果 filename为符号链接，则为真  [ -L /usr/bin/grep ]
-# -r filename  如果 filename可读，则为真  [ -r /var/log/syslog ]
-# -w filename  如果 filename可写，则为真  [ -w /var/mytmp.txt ]
-# -x filename  如果 filename可执行，则为真  [ -L /usr/bin/grep ]
-# filename1 -nt filename2  如果 filename1比 filename2新，则为真  [ /tmp/install/etc/services -nt /etc/services ]
-# filename1 -ot filename2  如果 filename1比 filename2旧，则为真  [ /boot/bzImage -ot arch/i386/boot/bzImage ]
