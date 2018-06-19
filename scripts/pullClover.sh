53898b81f8e183beb584498a8d016c80244438a4
liuzq <liuzq@agentool.com>
2018-06-19 17:59:01 +0800

    add permision judgement

--- scripts/pullClover.sh
+++ scripts/pullClover.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 # target folder: 与整个项目同级的clover
+if [ ! -x "../scripts/pullClover.sh" ];then
+echo 'please enter: sodu chmod 777 ../scripts/pullClover.sh'
+fi
 
 if [ ! -d "../clover" ];then
 # 没有则创建文件夹，拉取github仓库
@@ -16,5 +19,12 @@ cd ../clover
 git pull
 fi
 
-echo 'clover pulled the latest git verion.'
-
+# -e filename  如果 filename存在，则为真  [ -e /var/log/syslog ]
+# -d filename  如果 filename为目录，则为真  [ -d /tmp/mydir ]
+# -f filename  如果 filename为常规文件，则为真  [ -f /usr/bin/grep ]
+# -L filename  如果 filename为符号链接，则为真  [ -L /usr/bin/grep ]
+# -r filename  如果 filename可读，则为真  [ -r /var/log/syslog ]
+# -w filename  如果 filename可写，则为真  [ -w /var/mytmp.txt ]
+# -x filename  如果 filename可执行，则为真  [ -L /usr/bin/grep ]
+# filename1 -nt filename2  如果 filename1比 filename2新，则为真  [ /tmp/install/etc/services -nt /etc/services ]
+# filename1 -ot filename2  如果 filename1比 filename2旧，则为真  [ /boot/bzImage -ot arch/i386/boot/bzImage ]
