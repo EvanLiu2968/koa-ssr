16f9a03763803026d8a3f52c5435480782e687ca
evanliu2968 <296823596@qq.com>
2018-06-19 17:46:42 +0800

    Add shell for create clover

--- scripts/pullClover.sh
+++ scripts/pullClover.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
 # target folder: 与整个项目同级的clover
-cd ../clover
 
-# pull clover
+if [ ! -d "../clover" ];then
+# 没有则创建文件夹，拉取github仓库
+echo 'clover is not found, start to create clover files...'
+mkdir ../clover
+cd ../clover
+git init
+git remote add clover https://github.com/EvanLiu2968/clover.git
+git fetch clover
+git checkout master
+else
+cd ../clover
 git pull
+fi
+
+echo 'clover pulled the latest git verion.'
+
