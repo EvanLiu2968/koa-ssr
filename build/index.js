a985dc4cb2dd045e4ac9b415de8f4badcdf83a4a
evanliu2968 <evanliu2968@gmail.com>
2018-09-14 00:02:10 +0800

    优化dev环境nodemon的启动

--- build/index.js
+++ build/index.js
@@ -4,6 +4,9 @@ const path = require('path')
 const chalk = require('chalk')
 const util = require('./util')
 const config = require('../config')
+const nodemon = require('nodemon')
+const WebpackDevServer = require('webpack-dev-server');
+const WebpackDevServerOutput = require('./plugins/webpack-dev-server-output');
 
 util.runCmd('sh', [path.join(config.scriptsDir,'clean.sh')], function(res){
   console.log(chalk.green(res + ' \n'))
@@ -13,7 +16,6 @@ util.runCmd('sh', [path.join(config.scriptsDir,'pullClover.sh')], function(res){
 });
 
 if (process.env.APP_ENV == 'dev') {
-  const WebpackDevServer = require('webpack-dev-server');
   const webpackDevConfig = require('./webpack.dev.config');
   const compiler = webpack(webpackDevConfig)
   const server = new WebpackDevServer(compiler, webpackDevConfig.devServer);
@@ -25,14 +27,26 @@ if (process.env.APP_ENV == 'dev') {
     console.log(chalk.cyan('  Compilation complete.\n'))
     callback();
   })
+
   if(config.serverSideRendering){
     const webpackServerConfig = require('./webpack.server.config');
+    webpackServerConfig.plugins.unshift(new WebpackDevServerOutput(config.static.dir));
     const compiler = webpack(webpackServerConfig);
     new WebpackDevServer(compiler, webpackDevConfig.devServer).listen(webpackDevConfig.devServer.port+1, 'localhost', function (err, result) {
       if (err) throw err;
       console.log(`server restarting...`);
     })
+    let restart = false
+    compiler.plugin('emit', function (compilation, callback) {
+      console.log(chalk.cyan('view for server  Compilation complete.\n'))
+      callback();
+      if(!restart){ //为了避免view没有生成之前nodemon就已经启动的情况，后续则需要手动触发app的change重启server
+        nodemon.emit('restart')
+      }
+      restart = true;
+    })
   }
+  require('./nodemon')
 } else {
   const webpackProdConfig = require('./webpack.prod.config');
   const spinner = ora('编译中...')
--- index.js
+++ index.js
@@ -4,45 +4,6 @@
 
 if(process.env.APP_ENV == 'dev'){
   require('./build')
-
-  const nodemon = require('nodemon');
-  const chalk = require('chalk');
-
-  nodemon({
-    "execMap": {
-      "js": 'node --harmony ./app/core/index.js'
-    },
-    "script": './app/core/index.js',
-    "ext": 'js html',
-    "restartable": "rs",
-    "verbose": true,
-    "ignore": [
-      "public/*",
-      "dist/*",
-      ".git/*",
-      "*.db"
-    ],
-    "watch": [
-      "app/*",
-    ],
-  });
-  
-  nodemon.on('start', function () {
-    let log = 'server is starting';
-    console.log(chalk.green(log + ' \n'))
-  })
-  .on('quit', function () {
-    let log = 'server has quited';
-    console.log(chalk.red('\n' + log + ' \n'))
-    process.exit();
-  })
-  .on('restart', function (files) {
-    if (!files) {
-      return false
-    }
-    let log = `${files} changed`;
-    console.log(chalk.yellow('\n' + log + ' \n'))
-  });
 }else{
   require('./app/core')
 }
